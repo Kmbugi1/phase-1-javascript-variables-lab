fbca0ca1aa52a4075bc8126b3bc48cd87e712ef1
keith <keith.wachira@students.moringaschol.com>
2023-10-03 14:15:36 +0300

    attempt one

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const { expect } = require("chai")
+
 // Code your solution in this file!
+describe('index.js ,function()'){
+    // there's stuff in here , too
+};
+it('is set as scuber', function()){
+    //tests are here 
+};
+it('is set as scuber',function()){
+    expect(kmt).to.equal('scuber');
+};
